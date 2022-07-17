2e9756a023f3da7368bc9e2efec0d13c6ab6008b
syduarte <syduarte@uniguajira.edu.co>
2022-07-17 04:57:15 -0500

    Commit inicial web mark page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import * as Aos from 'aos';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webmark';
+
+   ngOnInit() {
+   
+    console.log('AOS');
+    Aos.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  
+  }
+
 }
